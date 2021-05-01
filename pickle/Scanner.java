2401231c189dd4e19bdbcf49645c7e6aee146298
Caleb Scott <calebcscott@outlook.com>
2021-04-30 19:50:03 -0500

    Add slice token to scanner

--- pickle/Scanner.java
+++ pickle/Scanner.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 
 public class Scanner {
 
-    private final static String delimiters = " \t:;()'\"=!<>+-*/[]#,^\n";   // Terminate a token
-    private final static String operators = "+-*/<>=!^#";                   // All operators
+    private final static String delimiters = " \t:;()'\"=!<>+-*/[]#~,^\n";   // Terminate a token
+    private final static String operators = "+-*/<>=!^#~";                   // All operators
     private final static String separators = "(),:;[]";                     // All seperators
 
     protected ArrayList<String> sourceLineM;                                // List of all source file lines
@@ -490,6 +490,9 @@ public class Scanner {
 
     public void classifyOperator(String tokenStr) {
         switch (tokenStr) {
+            case "~":
+                nextToken.operatorPrecedence = OperatorPrecedence.SLICE;
+                break;
             case "-":
                 if (currentToken.primClassif != Classif.OPERAND && !currentToken.tokenStr.equals(")") && !currentToken.tokenStr.equals("]")) {
                     nextToken.operatorPrecedence = OperatorPrecedence.UNARYMINUS;
