bbf5ad694caabd07b5ee27fc5d39e393acaf9a0f
LukeCD <degoes.luke@Gmail.com>
2021-05-03 00:15:28 -0500

    added Bool tests

--- documentation/combineTestsToSingleFile.py
+++ documentation/combineTestsToSingleFile.py
@@ -1,5 +1,11 @@
 import sys
 import os
+import re
+
+def sorted_alphanumeric(data):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(data, key=alphanum_key)
 
 if len(sys.argv) != 3: 
     print("args: pathToTestDirectory textFileToCreate")
@@ -12,7 +18,7 @@ if not os.path.exists(sys.argv[1]):
 
 f = open(os.path.join("./tests/", sys.argv[2]), 'w')
 
-for file in os.listdir(sys.argv[1]):
+for file in sorted_alphanumeric(os.listdir(sys.argv[1])):
     if file.endswith(".txt"):
         f2 = open(os.path.join(sys.argv[1], file))
         f.write("// " + file)
